9e5424a13c6e762025878d7605fcc8d439bbf0bd
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2009-02-05 00:44:34 +0000

    - Added SOAP Part with compiling and creating   service.xml from Java code. - Added SupoSE WeFe Module
    git-svn-id: http://svn.serverix/supose/branches/B_0.6.0@208 2a5690f4-4b40-0410-ba62-85a317ce182b

---
+++ supose-soap/src/main/java/com/soebes/supose/soap/SearchResult.java
@@ -0,0 +1,24 @@
+package com.soebes.supose.soap;
+
+public class SearchResult {
+
+	private Long revision;
+	private String fileName;
+
+	public SearchResult() {
+	}
+
+	public Long getRevision() {
+		return revision;
+	}
+	public void setRevision(Long revision) {
+		this.revision = revision;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+ 
+}
---
+++ supose-soap/src/main/java/com/soebes/supose/soap/SupoSEService.java
@@ -0,0 +1,11 @@
+package com.soebes.supose.soap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupoSEService {
+
+	public List<SearchResult> search (String queryString) {
+		return new ArrayList<SearchResult>();
+	}
+}
