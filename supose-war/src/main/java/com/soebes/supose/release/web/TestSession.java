8b703cf278a466e547d6ceee7652301d15465a82
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2009-06-06 20:59:04 +0000

    - Updated Session for Baked beans in JSF - Tried to fix resource bundle problem, but this is not working yet.
    git-svn-id: http://svn.serverix/supose/branches/B_0.7.0@361 2a5690f4-4b40-0410-ba62-85a317ce182b

--- supose-war/src/main/java/com/soebes/supose/release/web/TestSession.java
+++ supose-war/src/main/java/com/soebes/supose/release/web/TestSession.java
@@ -1,5 +1,22 @@
 package com.soebes.supose.release.web;
 
-public class TestSession {
+import java.io.Serializable;
+
+public class TestSession implements Serializable {
+
+	private static final long serialVersionUID = 120672378912357403L;
+
+	private String query;
+
+	public TestSession() {
+	}	
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public String getQuery() {
+		return query;
+	}
 
 }
