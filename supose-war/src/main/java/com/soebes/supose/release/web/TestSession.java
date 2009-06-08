9ee1949e601fba6c4095de189774feab86882aa3
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2009-06-08 19:21:31 +0000

    - Now changed navigation rule in JSF to start with the first page   after a query.
    git-svn-id: http://svn.serverix/supose/branches/B_0.7.0@364 2a5690f4-4b40-0410-ba62-85a317ce182b

--- supose-war/src/main/java/com/soebes/supose/release/web/TestSession.java
+++ supose-war/src/main/java/com/soebes/supose/release/web/TestSession.java
@@ -2,7 +2,10 @@ package com.soebes.supose.release.web;
 
 import java.io.Serializable;
 
+import org.apache.log4j.Logger;
+
 public class TestSession implements Serializable {
+	private static Logger LOGGER = Logger.getLogger(TestSession.class);
 
 	private static final long serialVersionUID = 120672378912357403L;
 
@@ -18,5 +21,13 @@ public class TestSession implements Serializable {
 	public String getQuery() {
 		return query;
 	}
+
+	public String query() {
+		LOGGER.info("query() we start");
+		LOGGER.info("The query:" + getQuery());
+		//Do some things here...
+		return "SUCCESS";
+	}
+	
 
 }
