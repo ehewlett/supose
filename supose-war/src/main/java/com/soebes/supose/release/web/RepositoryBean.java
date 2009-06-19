138eecb5c12f65e2ead703dca830bd75634f27c4
kama <kama@2a5690f4-4b40-0410-ba62-85a317ce182b>
2009-06-19 14:02:44 +0000

    - Added missing RepositoryBean file.
    git-svn-id: http://svn.serverix/supose/branches/B_0.7.0@392 2a5690f4-4b40-0410-ba62-85a317ce182b

---
+++ supose-war/src/main/java/com/soebes/supose/release/web/RepositoryBean.java
@@ -0,0 +1,14 @@
+package com.soebes.supose.release.web;
+
+public class RepositoryBean {
+	private String index;
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+}
